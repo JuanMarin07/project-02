ad08c0e2232448507868c477852f4c2b6ffcd570
juanmarin07 <camilopuerta9205@gmail.com>
2016-06-14 20:42:28 -0500

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+function greetings() {
+  window.alert('Hi there, you want to log in !');
+};
+
+function validate () {
+  var username, password
+
+  username = document.getElementById('username').value
+  password = document.getElementById('password').value
+
+  if ((username === undefined || username === '')
+                  || (password === undefined || password === '')) {
+    document.getElementById('error').innerHTML = 'Username or Password are invalid !.'
+      document.getElementById('error').style.color = "red"
+  } else {
+    document.getElementById('error').style.color = "green"
+    document.getElementById('error').innerHTML = 'Username and Password are valid !.'
+  };
+};
